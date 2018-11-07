7e8f4eb0b2575f4f315184a177326a30bf3813f9
Tabea Rettelbach <tabea.rettelbach@hotmail.de>
2018-11-07 09:20:06 +0800

    added comments

--- scripts/cnn_pytorch_emc_v2.py
+++ scripts/cnn_pytorch_emc_v2.py
@@ -13,26 +13,24 @@ from time import gmtime, strftime
 #import mylogger
 import mytransforms
 import myplotting
-from StringIO import StringIO
+# from StringIO import StringIO
 
-#try:
-#    from StringIO import StringIO  # Python 2.7
-#except ImportError:
-#    from io import BytesIO  # Python 3.x
-
-NLABEL = int(5)
+# Hyperparameter variables
+NLABEL = int(4)
 BATCH_SIZE = 1
 EPOCHS = 3
 INIT_LR = 5e-7
 
+# check if GPU available
 use_gpu = torch.cuda.is_available()
 # use_gpu = False
 print(use_gpu)
 
+# make sure it runs on GPU, if available
 device = torch.device("cuda:0" if use_gpu else "cpu")
 print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
-
+# choose data augmentation elements
 transform = transforms.Compose([
     mytransforms.CenterCropFits(800),
     # CutRangeFits(0),
@@ -65,10 +63,12 @@ class MyCustomDatasetFits(data.Dataset):
         # Get image name from the pandas df
         single_image_name = self.image_arr[index]
 
+        # open the fits data
         data1 = pyfits.open(single_image_name, axes=2)
         data2 = data1[0].data.astype('float32')
         data3 = data2.reshape(4000, 4000, 1)
 
+        # standardize my images
         img_mean = np.ndarray.mean(data3)
         img_std = np.std(data3)
         img_stand = (data3 - img_mean)/img_std
@@ -96,82 +96,167 @@ class CNN(nn.Module):
     def __init__(self, nlabel):
         super(CNN, self).__init__()
         self.nlabel = nlabel
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2d(2))
-        # print(self.layer1)
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2d(2))
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=7, padding=3),
-            nn.ReLU(),
-            nn.MaxPool2d(2))
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2))
+
+        # all building blocks for the network
+        self.layer1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=15, padding=7)
+        self.layer2 = nn.ReLU()
+        self.layer3 = nn.MaxPool2d(3)
+
+        self.layer4 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=9, padding=4)
         self.layer5 = nn.ReLU()
-        self.fc1 = nn.Linear(80000, 100)
+        self.layer6 = nn.MaxPool2d(3)
+
+        self.layer7 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, padding=2)
+        self.layer8 = nn.ReLU()
+        self.layer9 = nn.MaxPool2d(3)
+
+        # self.layer1 = nn.Sequential(
+        #     nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, padding=2),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(2))
+        # # print(self.layer1)
+        # self.layer2 = nn.Sequential(
+        #     nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, padding=2),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(2))
+        # self.layer3 = nn.Sequential(
+        #     nn.Conv2d(in_channels=32, out_channels=32, kernel_size=7, padding=3),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(2))
+        # self.layer4 = nn.Sequential(
+        #     nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(2))
+
+        # self.layer5 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1)
+
+        self.fc1 = nn.Linear(6272, 100)
         self.fc2 = nn.Linear(100, 50)
         self.fc3 = nn.Linear(50, NLABEL)
-        # self.apply(weights_init)
         self.dropout = nn.Dropout(p=0.1)
+
         self.fc1 = nn.DataParallel(self.fc1)
         self.fc2 = nn.DataParallel(self.fc2)
         self.fc3 = nn.DataParallel(self.fc3)
 
     def forward(self, x):
+        # defining the architecture of the network
         out = self.layer1(x)
-        out1 = out[0][0].cpu().detach().numpy()
+        # for visualizing the filters
+        a = self.layer1.weight[0][0]
+        b = a.cpu().detach().numpy()
+        weights.append(b)
+
         out = self.layer2(out)
-        out2 = out[0][0].cpu().detach().numpy()
         out = self.layer3(out)
-        out3 = out[0][0].cpu().detach().numpy()
+
         out = self.layer4(out)
-        out4 = out[0][0].cpu().detach().numpy()
+        a = self.layer4.weight[0][0]
+        b = a.cpu().detach().numpy()
+        weights.append(b)
+
         out = self.layer5(out)
-        # print(out.weight)
-        out = out.view(out.size(0), -1)  # flattening... out.view(BATCH_SIZE, -1)
-        
-        out = self.fc1(out)  # fully connected layers at the end
+        out = self.layer6(out)
+
+        out = self.layer7(out)
+        a = self.layer7.weight[0][0]
+        b = a.cpu().detach().numpy()
+        weights.append(b)
+
+        out = self.layer8(out)
+        out = self.layer9(out)
+
+        # for visualizing the images
+        out4pic = out[0][0].cpu().detach().numpy()
+        pictures.append(out4pic)
+
+        # flatten layers
+        out = out.view(out.size(0), -1)
+        # fully connected layers at the end (with dropout)
+        out = self.fc1(out)
         out = self.dropout(out)
         out = self.fc2(out)
         out = self.fc3(out)
-        out = self.dropout(out)
-        pictures.append(out4)
-        # show_images(pictures, 2, titles=['1', '2', '3', '4'])
-        # plt.show()
-        return out, pictures
 
-# instance of the Conv Net
+        return out, pictures, weights 
 
-# model = CNN(NLABEL)
+
+
+#        super(CNN, self).__init__()
+#        self.nlabel = nlabel
+#        self.layer1 = nn.Sequential(
+#            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, padding=2),
+#            nn.ReLU(),
+#            nn.MaxPool2d(2))
+#        # print(self.layer1)
+#        self.layer2 = nn.Sequential(
+#            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, padding=2),
+#            nn.ReLU(),
+#            nn.MaxPool2d(2))
+#        self.layer3 = nn.Sequential(
+#            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=7, padding=3),
+#            nn.ReLU(),
+#            nn.MaxPool2d(2))
+#        self.layer4 = nn.Sequential(
+#            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+#            nn.ReLU(),
+#            nn.MaxPool2d(2))
+#        self.layer5 = nn.ReLU()
+#        self.fc1 = nn.Linear(80000, 100)
+#        self.fc2 = nn.Linear(100, 50)
+#        self.fc3 = nn.Linear(50, NLABEL)
+#        # self.apply(weights_init)
+#        self.dropout = nn.Dropout(p=0.1)
+#        self.fc1 = nn.DataParallel(self.fc1)
+#        self.fc2 = nn.DataParallel(self.fc2)
+#        self.fc3 = nn.DataParallel(self.fc3)
+#
+#    def forward(self, x):
+#        out = self.layer1(x)
+#        out1 = out[0][0].cpu().detach().numpy()
+#        out = self.layer2(out)
+#        out2 = out[0][0].cpu().detach().numpy()
+#        out = self.layer3(out)
+#        out3 = out[0][0].cpu().detach().numpy()
+#        out = self.layer4(out)
+#        out4 = out[0][0].cpu().detach().numpy()
+#        out = self.layer5(out)
+#        # print(out.weight)
+#        out = out.view(out.size(0), -1)  # flattening... out.view(BATCH_SIZE, -1)
+#        
+#        out = self.fc1(out)  # fully connected layers at the end
+#        out = self.dropout(out)
+#        out = self.fc2(out)
+#        out = self.fc3(out)
+#        out = self.dropout(out)
+#        pictures.append(out4)
+#        # show_images(pictures, 2, titles=['1', '2', '3', '4'])
+#        # plt.show()
+#        return out, pictures
+
+        
+
+# make instance of the Conv Net, send to GPU if available
 model = CNN(NLABEL).to(device)
-#logger = mylogger.Logger('./logs')
-
 if use_gpu:
     model = model.cuda()
 
+# define criterion and optimizer
 criterion = nn.CrossEntropyLoss(reduce=True)
 optimizer = torch.optim.Adam(model.parameters(), lr=INIT_LR)
 
+# prepare some lists for statistical evaluation
 train_losses = []
 test_losses = []
-
 train_accs = []
 test_accs = []
-
-
 train_true = []
 train_pred = []
-
 test_true = []
 test_pred = []
 
 
+# run the data through the network for x epochs
 for epoch in range(EPOCHS):
     model.train()
     correct_train = 0
@@ -212,36 +297,10 @@ for epoch in range(EPOCHS):
     train_accs.append(100*correct_train/1047)
 #    print(losses)
 
-    # set network to evaluation mode (no more dropout, fi)
+    # set network to evaluation mode (no more dropout, fe)
     model.eval()
     correct = 0
     total = 0
-
-    # ================================================================== #
-    #                        Tensorboard Logging                         #
-    # ================================================================== #
-
-    # 1. Log scalar values (scalar summary)
-    #info = {'loss': float(str(loss.data[0])[7:-18]), 'accuracy': (100*correct_train/12)}
-
-    #for tag, value in info.items():
-    #    logger.scalar_summary(tag, value, epoch + 1)
-
-    # 2. Log values and gradients of the parameters (histogram summary)
-    #for tag, value in model.named_parameters():
-    #    tag = tag.replace('.', '/')
-    #    logger.histo_summary(tag, value.data.cpu().numpy(), epoch + 1)
-    #    logger.histo_summary(tag + '/grad', value.grad.data.cpu().numpy(), epoch + 1)
-
-    # 3. Log training images (image summary)
-    #info = {'images': images.view(-1, 800, 800)[:10].cpu().numpy()}
-
-    #for tag, images in info.items():
-    #    logger.image_summary(tag, images, epoch + 1)
-
-    # ================================================================== #
-    #                      Tensorboard Logging Over                      #
-    # ================================================================== #
 
     for images, labels, location in testloader:
         if use_gpu:
@@ -279,10 +338,8 @@ print('train_pred: ', train_pred)
 print('test_true: ', test_true)
 print('test_pred: ', test_pred)
 
-show_images(pictures[24:], 3)  #, titles=['1', '2', '3', '4'])
-
-# https://github.com/utkuozbulak/pytorch-cnn-visualizations/blob/master/src/cnn_layer_visualization.py
-# das koennte funktionieren... mal Model speichern und ausprobieren
+show_images(pictures[24:], 3)
+show_images(weights[-24:], 3)
 
 #print(precision_recall_fscore_support(test_true, test_pred))
 #cm = confusion_matrix(test_true, test_pred)
